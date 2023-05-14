b6d67a8d26bb698cf008f5070252d73761d239c3
xiAMLEO <leonardobonanni002@gmail.com>
2023-05-15 01:50:17 +0200

    edits for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   images: {
-    loader: 'akamai',
-    path: '',
+    loader: "akamai",
+    path: "",
   },
-  assetPrefix: './',
-}
+  assetPrefix: "./",
+};
 
 export default nextConfig;
